98bcb5e45f2734ca53fe85017912d59f02e320f2
Johan Elmström <johan.elmstrom@gmail.com>
2015-09-25 16:38:19 +0200

    CORS filter

--- src/main/java/se/aptitud/aptifootball/applicaton/AptiFootball.java
+++ src/main/java/se/aptitud/aptifootball/applicaton/AptiFootball.java
@@ -4,18 +4,31 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import se.aptitud.aptifootball.player.PlayerResource;
 import se.aptitud.aptifootball.user.UserResource;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+import java.util.EnumSet;
 
 public class AptiFootball extends Application<AptiFootballConfig> {
 
     public static void main(String[] args) throws Exception {
         new AptiFootball().run(args);
     }
+
     @Override
     public void run(AptiFootballConfig aptiFootballConfig, Environment environment) throws Exception {
-            environment.jersey().register(new UserResource());
-            environment.jersey().register(new PlayerResource(aptiFootballConfig));
+        environment.jersey().register(new UserResource());
+        environment.jersey().register(new PlayerResource(aptiFootballConfig));
+
+        final Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 
 
